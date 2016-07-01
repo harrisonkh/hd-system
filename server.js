311c87d82ab2b7567c3e171351c774c7642de737
Harrison Kusevskis-Hayes <Harrison Kusevskis-Hayes>
2016-07-01 15:13:26 +1000

    password checking, input form, data validation

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var fs = require('fs');
 var DB_FILE_NAME = 'database.db';
 var SECRET_KEY = 'notsosecret';
 var db = new sqlite3.Database(DB_FILE_NAME);
+var btoa = require('btoa');
+var atob = require('atob');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,15 +19,21 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 
 app.post('/login', function(req,res){
-
+console.log(req);
 var username = req.body.username;
-var pass = req.body.username;
+var pass = req.body.password;
 console.log(username);
 console.log(pass);
 
 db.serialize(function() {
 	db.each("SELECT password FROM user WHERE name='" + username + "'", function(err, row) {
-		console.log("found password",decrypt(row.Password));
+		var dbPass =decrypt(row.Password,SECRET_KEY);
+		console.log("DB Pass :",dbPass);
+		if (pass == dbPass){
+			res.sendFile(__dirname + "/main.html");
+		}else{
+			res.end("INCORRECT");
+		}
   });
   
 });
@@ -43,6 +51,20 @@ app.listen(3000, function () {
 function encrypt(toEncrypt){
 	return CryptoJS.AES.encrypt(toEncrypt, SECRET_KEY).toString();
 }
-function decrypt(hash){
-	return CryptoJS.AES.decrypt(hash, SECRET_KEY).toString();
+function decrypt(hash,key){
+
+var decrypted = CryptoJS.AES.decrypt(
+        hash,key,
+        {   
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        }
+    );
+return decrypted.toString(CryptoJS.enc.Utf8);
+}
+function hex2a(hex) {
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
 }
